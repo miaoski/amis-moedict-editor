de3f19ea0e8da99c730c243e08b724c1b9e2ad7f
ETBlue <etblue.tw@gmail.com>
2022-03-14 06:06:22 +0800

    fix: specify css naming scope

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -11,7 +11,9 @@ const queryClient = new QueryClient();
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <section id="amis-moeditor">
+        <App />
+      </section>
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
